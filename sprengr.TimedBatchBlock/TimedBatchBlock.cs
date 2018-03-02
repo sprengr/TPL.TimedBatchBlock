a3c5e79224d1c1ea6a1bbe46cb1ff4dd94addeae
Sebi <sebastian.sprenger@ascarix.ch>
2018-03-02 17:40:16 +0100

    Created an IPropagator with a transformblock (doing the countdown) and a batchblock

---
+++ sprengr.TimedBatchBlock/TimedBatchBlock.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace sprengr.TimedBatchBlock
+{
+    public class TimedBatchBlock<T> : IPropagatorBlock<T, T[]>
+    {
+        private readonly ISourceBlock<T[]> _batchBlock;
+        private readonly ITargetBlock<T> _timeoutBlock;
+        private readonly Timer _triggerBatchTimer;
+
+        public TimedBatchBlock(int batchSize, int triggerDueTimeInMs = 5000)
+        {
+            var batchBlock = new BatchBlock<T>(batchSize);
+            _triggerBatchTimer = new Timer((state) => batchBlock.TriggerBatch());
+            var timeoutBlock = new TransformBlock<T, T>((value) =>
+            {
+                _triggerBatchTimer.Change(triggerDueTimeInMs, Timeout.Infinite);
+                return value;
+            });
+
+            timeoutBlock.LinkTo(batchBlock);
+
+            _batchBlock = batchBlock;
+            _timeoutBlock = timeoutBlock;
+        }
+
+        public Task Completion => _batchBlock.Completion;
+
+        public void Complete()
+        {
+            _timeoutBlock.Complete();
+            _batchBlock.Complete();
+        }
+
+        public T[] ConsumeMessage(DataflowMessageHeader messageHeader, ITargetBlock<T[]> targets, out bool messageConsumed)
+        {
+            return _batchBlock.ConsumeMessage(messageHeader, targets, out messageConsumed);
+        }
+
+        public void Fault(Exception exception)
+        {
+            _batchBlock.Fault(exception);
+        }
+
+        public IDisposable LinkTo(ITargetBlock<T[]> target, DataflowLinkOptions linkOptions)
+        {
+            return _batchBlock.LinkTo(target, linkOptions);
+        }
+
+        public DataflowMessageStatus OfferMessage(DataflowMessageHeader messageHeader, T messageValue, ISourceBlock<T> source, bool consumeToAccept)
+        {
+            return _timeoutBlock.OfferMessage(messageHeader, messageValue, source, consumeToAccept);
+        }
+
+        public void ReleaseReservation(DataflowMessageHeader messageHeader, ITargetBlock<T[]> target)
+        {
+            _batchBlock.ReleaseReservation(messageHeader, target);
+        }
+
+        public bool ReserveMessage(DataflowMessageHeader messageHeader, ITargetBlock<T[]> target)
+        {
+            return _batchBlock.ReserveMessage(messageHeader, target);
+        }
+    }
+}
